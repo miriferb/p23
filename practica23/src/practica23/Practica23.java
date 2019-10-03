5ad8e5413495651fc1e77a78eddb1b31650cb561
18PROGB0173 <18PROGB0173@NAS94.labs.cbtis94.local>
2019-10-03 12:01:06 -0600

    se creo proyecto base

---
+++ practica23/src/practica23/Practica23.java
@@ -0,0 +1,17 @@
+
+package practica23;
+
+/**
+ *
+ * @author 18PROGB0173
+ */
+public class Practica23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
