4c149cd0a74a667b3f79a3784b1b39c8a521b403
18PROGB0173 <18PROGB0173@NAS94.labs.cbtis94.local>
2019-10-28 13:26:06 -0600

    se creo proyecto base

---
+++ p23/src/p23/P23.java
@@ -0,0 +1,17 @@
+
+package p23;
+
+/**
+ *
+ * @author 18progb0173
+ */
+public class P23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
